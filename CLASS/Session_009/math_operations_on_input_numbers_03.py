cb3eb1e902f24dab84a141b0d20a995a01d8d383
Pranav Waikar <pranavwaikar@gmail.com>
2025-02-24 19:07:23 +0530

    Create math_operations_on_input_numbers_03.py

---
+++ CLASS/Session_009/math_operations_on_input_numbers_03.py
@@ -0,0 +1,22 @@
+print('Math operations on integers')
+
+s1 = input('Enter number one: ')
+n1 = int(s1)
+
+s2 = input('Enter number two')
+n2 = int(s2)
+
+#Perform addition, subtraction, multiplication, division, power and store results in different variables
+
+result_of_addition = n1 + n2
+result_of_subtraction = n1 - n2
+result_of_multiplication = n1 * n2
+result_of_division = n1 / n2
+result_of_power = n1 ** n2
+
+# Print results
+print('Addition:',result_of_addition)
+print('Subtraction:',result_of_subtraction)
+print('Multiplication:',result_of_multiplication)
+print('Division:',result_of_division)
+print('Power:',result_of_power)
