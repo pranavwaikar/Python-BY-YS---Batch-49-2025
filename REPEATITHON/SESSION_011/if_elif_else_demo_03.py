87ba4f90ff48eaac61734aee02de6b351fa92ef1
pranavwaikar <pranavwaikar@gmail.com>
2025-02-25 07:41:32 +0530

    repeatithon codes commit

--- REPEATITHON/SESSION_011/if_elif_else_demo_03.py
+++ REPEATITHON/SESSION_011/if_elif_else_demo_03.py
@@ -1,7 +1,7 @@
 print('start of program')
 n_pw_3= int(input('Enter an_pw_3integer:'))
 if n_pw_3> 0:
-    print(n, 'is a positive number')
+    print(n_pw_3, 'is a positive number')
 elif n_pw_3< 0:
     print(n_pw_3, 'is a negative number')
 else:
