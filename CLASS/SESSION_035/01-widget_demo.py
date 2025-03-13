66b132c102359068002a4f23aa2c544684517d3f
pranavwaikar <pranavwaikar@gmail.com>
2025-03-13 07:26:21 +0530

    button on ui

---
+++ CLASS/SESSION_035/01-widget_demo.py
@@ -0,0 +1,33 @@
+# Import required GUI package
+import tkinter
+import tkinter.ttk
+import sys
+
+# Handler functions
+def terminate():
+    print("Exiting app...")
+    sys.exit(0)
+
+# Create a main window by making an object of Tk class
+root_window = tkinter.Tk()
+
+# Add title of Window
+root_window.title('CPA - Python 49 batch Window - Widget Demo')
+
+master_frame = tkinter.ttk.Frame(root_window, padding="3 3 12 12")
+master_frame.grid(row=0, column=0, sticky=(tkinter.N, tkinter.W, tkinter.E, tkinter.S))
+
+B = tkinter.Button(master_frame)
+B.configure(text="Exit Button", command=terminate)
+B.grid(row=1, column=1, sticky=tkinter.E)
+
+
+# Set minimumn Width & height
+root_window.minsize(400,300)
+
+# Set max Width & height
+root_window.maxsize(1600,1200)
+
+# Enter event loop
+root_window.mainloop()
+
