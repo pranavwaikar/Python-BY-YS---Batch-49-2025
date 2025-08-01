62b75a1700670fb991fb20e92adce71b8ce41834
pranavwaikar <pranavwaikar@gmail.com>
2025-08-02 01:38:47 +0530

    decorators

--- decorator-impl/logger.py
+++ decorator-impl/logger.py
@@ -13,42 +13,43 @@ def genericExceptionHandler(tb=False, doExit=False, fileHandle=sys.stdout):
         sys.exit(0)
 
 
-def logger(functionObject):
-    def innerFunction(*args, **kwargs):
-        # Obtaining the log file handle
-        logFileHandle = None
-        try:
-            logFileHandle = open("log.txt", "a")
-        except Exception:
-            genericExceptionHandler(doExit=False)
+def logger(log_filename):
+    def decorator(functionObject):
+        def wrapperFunction(*args, **kwargs):
+            logFileHandle = None
+            try:
+                logFileHandle = open(log_filename, "a")
+            except Exception:
+                genericExceptionHandler(doExit=False)
 
-        assert logFileHandle is not None, "Some deeper issue with log file handle"
+            assert logFileHandle is not None, "Some deeper issue with log file handle"
 
-        # Preprocessing start
-        line = '-' * 40 + '\n'
-        print(line, functionObject.__name__, file=logFileHandle)
-        print('Time of call', time.ctime(time.time()), file=logFileHandle)
+            # Preprocessing start
+            line = '-' * 40 + '\n'
+            print(line, functionObject.__name__, file=logFileHandle)
+            print('Time of call', time.ctime(time.time()), file=logFileHandle)
 
-        if len(args) > 0:
-            for i in range(len(args)):
-                print(f"args[{i}]: {args[i]}", file=logFileHandle)
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                print(f"kwargs[{key}]: {value}", file=logFileHandle)
-        # Preprocessing end
+            if len(args) > 0:
+                for i in range(len(args)):
+                    print(f"args[{i}]: {args[i]}", file=logFileHandle)
+            if len(kwargs) > 0:
+                for key, value in kwargs.items():
+                    print(f"kwargs[{key}]: {value}", file=logFileHandle)
+            # Preprocessing end
 
-        try:
-            result = functionObject(*args, **kwargs)
-        except:
-            genericExceptionHandler(tb=True, doExit=False, fileHandle=logFileHandle)
-            result = None
+            try:
+                result = functionObject(*args, **kwargs)
+            except:
+                genericExceptionHandler(tb=True, doExit=False, fileHandle=logFileHandle)
+                result = None
 
-        # Postprocessing start
-        print("Result:", result, file=logFileHandle)
-        print('Time of return', time.ctime(time.time()), file=logFileHandle)
-        logFileHandle.flush()
-        logFileHandle.close()
-        # Postprocessing end
+            # Postprocessing start
+            print("Result:", result, file=logFileHandle)
+            print('Time of return', time.ctime(time.time()), file=logFileHandle)
+            logFileHandle.flush()
+            logFileHandle.close()
+            # Postprocessing end
 
-        return result
-    return innerFunction
+            return result
+        return wrapperFunction
+    return decorator
--- decorator-impl/test-logger.py
+++ decorator-impl/test-logger.py
@@ -1,7 +1,21 @@
 import logger
 
-@logger.logger
+# @logger.logger('my_log.txt')
+
+@logger.logger('my_log.txt')
 def sample_function(a, b, c=1):
     print("Inside sample_function")
     print(f"a: {a}, b: {b}, c: {c}")
     return a * b + c
+
+@logger.logger('my_log2.txt')
+def my_function(x, y):
+    print("Inside my_function")
+    print(f"x: {x}, y: {y}")
+    return x + y
+
+for i in range(2):
+    result = sample_function(i, i + 1, c=i + 2)
+    print(f"Result of sample_function: {result}")
+    result2 = my_function(i, i + 1)
+    print(f"Result of my_function: {result}")   
